ab8e97e0eb67c7119b574fae876a38b67bfa0ff7
tanjialin <tanjialin@heyakun.com>
2018-10-18 14:31:52 +0800

    initail commit

---
+++ miniprogram/api/address.js
@@ -0,0 +1,72 @@
+const d = new Date()
+const currentMonth = d.getMonth() + 1
+let season // Format: 2018-2019
+if (currentMonth >= 10) {
+    season = d.getFullYear().toString() + '-' + (d.getFullYear() + 1).toString().substring(2, 4)
+} else {
+    season = (d.getFullYear().toString() - 1) + '-' + d.getFullYear().toString().substring(2, 4)
+}
+
+const address = {
+    /**
+     * 某日所有的比赛
+     * @params gameDate: {String} {Format: yearmonthdate}
+     * @example gameDate: 20181125
+     */
+    gameGeneral: (gameDate) => {
+        return `https://data.nba.com/data/5s/json/cms/noseason/scoreboard/${gameDate}/games.json`
+    },
+    /**
+     * 比赛详情
+     * @params gameDate: {String} {Format: yearmonthdate} & gameId: {String}
+     * @example gameDate: 20151128 & gameId: 0021500239
+     */
+    gameDetail: (gameDate, gameId) => {
+        return `http://data.nba.com/data/10s/json/cms/noseason/game/${gameDate}/${gameId}/boxscore.json`
+    },
+
+    /**
+     * Current league standing
+     * @params year {String}
+     * @example year: 2018
+     */
+    leagueStanding: (year) => {
+        return `https://data.nba.com/data/json/cms/${year}/league/standings.json`
+    },
+    /**
+     * 所有球员列表
+     * @returns {string}
+     */
+    playerList: () => {
+        return `https://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=0&LeagueID=00&Season=${season}`
+    },
+
+    playerInfo: (id) => {
+        return `https://stats.nba.com/stats/commonplayerinfo?LeagueID=00&PlayerID=${id}&SeasonType=Regular+Season`
+    },
+
+    playerLog: (id) => {
+        return `https://stats.nba.com/stats/playergamelog?LeagueID=00&PerMode=PerGame&PlayerID=${id}&Season=${season}&SeasonType=Regular+Season`
+    },
+
+    /**
+     * @params gameDate month/date/year
+     */
+    teamRank: (gameDate) => {
+        return `https://stats.nba.com/stats/scoreboard?DayOffset=0&LeagueID=00&gameDate=${gameDate}`
+    },
+
+    teamInfo: (id) => {
+        return `https://stats.nba.com/stats/teaminfocommon?LeagueID=00&SeasonType=Regular+Season&TeamID=${id}&season=${season}`
+    },
+
+    teamDetail: (id) => {
+        return `https://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season=${season}&SeasonSegment=&SeasonType=Regular+Season&TeamID=${id}&VsConference=&VsDivision=`
+    },
+
+    teamDetailBasic: (id) => {
+        return `https://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=${season}&TeamID=${id}`
+    }
+}
+
+export default address
---
+++ miniprogram/config/team-map.js
@@ -0,0 +1,245 @@
+'use strict'
+
+export default {
+    atl: {
+        id: 1610612737,
+        city: 'Atlanta',
+        team: 'Hawks',
+        color: '#E03A3E',
+        conf: 'eastern',
+        logo: 'atl.png'
+    },
+    bkn: {
+        id: 1610612751,
+        city: 'Brooklyn',
+        team: 'Nets',
+        color: '#000',
+        conf: 'eastern',
+        logo: 'bkn.png'
+    },
+    bos: {
+        id: 1610612738,
+        city: 'Boston',
+        team: 'Celtics',
+        color: '#008348',
+        conf: 'eastern',
+        logo: 'bos.png'
+    },
+    cha: {
+        id: 1610612766,
+        city: 'Charlotte',
+        team: 'Hornets',
+        color: '#008CA8',
+        conf: 'eastern',
+        logo: 'cha.png'
+    },
+    chi: {
+        id: 1610612741,
+        city: 'Chicago',
+        team: 'Bulls',
+        color: '#CE1141',
+        conf: 'eastern',
+        logo: 'chi.png'
+    },
+    cle: {
+        id: 1610612739,
+        city: 'Cleveland',
+        team: 'Cavaliers',
+        color: '#860038',
+        conf: 'eastern',
+        logo: 'cle.png'
+    },
+    dal: {
+        id: 1610612742,
+        city: 'Dallas',
+        team: 'Mavericks',
+        color: '#007DC5',
+        conf: 'western',
+        logo: 'dal.png'
+    },
+    den: {
+        id: 1610612743,
+        city: 'Denver',
+        team: 'Nuggets',
+        color: '#FFB20F',
+        conf: 'western',
+        logo: 'den.png'
+    },
+    det: {
+        id: 1610612765,
+        city: 'Detroit',
+        team: 'Pistons',
+        color: '#006BB6',
+        conf: 'eastern',
+        logo: 'det.png'
+    },
+    gsw: {
+        id: 1610612744,
+        city: 'Golden State',
+        team: 'Warriors',
+        color: '#FDB927',
+        conf: 'western',
+        logo: 'gsw.png'
+    },
+    hou: {
+        id: 1610612745,
+        city: 'Houston',
+        team: 'Rockets',
+        color: '#CE1141',
+        conf: 'western',
+        logo: 'hou.png',
+        logo2: 'hou2.png'
+    },
+    ind: {
+        id: 1610612754,
+        city: 'Indiana',
+        team: 'Pacers',
+        color: '#FFC633',
+        conf: 'eastern',
+        logo: 'ind.png'
+    },
+    lac: {
+        id: 1610612746,
+        city: 'Los Angeles',
+        team: 'Clippers',
+        color: '#222',
+        conf: 'western',
+        logo: 'lac.png'
+    },
+    lal: {
+        id: 1610612747,
+        city: 'Los Angeles',
+        team: 'Lakers',
+        color: '#552582',
+        conf: 'western',
+        logo: 'lal.png'
+    },
+    mem: {
+        id: 1610612763,
+        city: 'Memphis',
+        team: 'Grizzlies',
+        color: '#6189B9',
+        conf: 'western',
+        logo: 'mem.png'
+    },
+    mia: {
+        id: 1610612748,
+        city: 'Miami',
+        team: 'Heat',
+        color: '#98002E',
+        conf: 'eastern',
+        logo: 'mia.png'
+    },
+    mil: {
+        id: 1610612749,
+        city: 'Milwaukee',
+        team: 'Bucks',
+        color: '#00471B',
+        conf: 'eastern',
+        logo: 'mil.png'
+    },
+    min: {
+        id: 1610612750,
+        city: 'Minnesota',
+        team: 'Timberwolves',
+        color: '#005083',
+        conf: 'western',
+        logo: 'min.png'
+    },
+    nop: {
+        id: 1610612740,
+        city: 'New Orleans',
+        team: 'Pelicans',
+        color: '#002B5C',
+        conf: 'western',
+        logo: 'nop.png'
+    },
+    nyk: {
+        id: 1610612752,
+        city: 'New York',
+        team: 'Knicks',
+        color: '#F58426',
+        conf: 'eastern',
+        logo: 'nyk.png'
+    },
+    okc: {
+        id: 1610612760,
+        city: 'Oklahoma City',
+        team: 'Thunder',
+        color: '#F05133',
+        conf: 'western',
+        logo: 'okc.png'
+    },
+    orl: {
+        id: 1610612753,
+        city: 'Orlando',
+        team: 'Magic',
+        color: '#007DC5',
+        conf: 'eastern',
+        logo: 'orl.png'
+    },
+    phi: {
+        id: 1610612755,
+        city: 'Philadelphia',
+        team: '76ers',
+        color: '#006BB6',
+        conf: 'eastern',
+        logo: 'phi.png'
+    },
+    phx: {
+        id: 1610612756,
+        city: 'Phoenix',
+        team: 'Suns',
+        color: '#E56020',
+        conf: 'western',
+        logo: 'phx.png'
+    },
+    por: {
+        id: 1610612757,
+        city: 'Portland',
+        team: 'Trail Blazers',
+        color: '#000',
+        conf: 'western',
+        logo: 'por.png'
+    },
+    sac: {
+        id: 1610612758,
+        city: 'Sacramento',
+        team: 'Kings',
+        color: '#724C9F',
+        conf: 'western',
+        logo: 'sac.png'
+    },
+    sas: {
+        id: 1610612759,
+        city: 'San Antonio',
+        team: 'Spurs',
+        color: '#B6BFBF',
+        conf: 'western',
+        logo: 'sas.png'
+    },
+    tor: {
+        id: 1610612761,
+        city: 'Toronto',
+        team: 'Raptors',
+        color: '#CE1141',
+        conf: 'eastern',
+        logo: 'tor.png'
+    },
+    uta: {
+        id: 1610612762,
+        city: 'Utah',
+        team: 'Jazz',
+        color: '#00471B',
+        conf: 'western',
+        logo: 'uta.png'
+    },
+    was: {
+        id: 1610612764,
+        city: 'Washington',
+        team: 'Wizards',
+        color: '#F5002F',
+        conf: 'eastern',
+        logo: 'was.png'
+    }
+}
